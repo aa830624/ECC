e2e0140d5ae72f3c1ff08fa67f3347fe70da7262
aa830624 <aa830624@gmail.com>
2015-10-19 14:02:58 +0800

    abc

---
+++ animate.js
@@ -0,0 +1,26 @@
+
+function sentMail(){
+
+  setTimeout(function(){document.getElementById("_1").style.display="inline"},500);
+  setTimeout(function(){document.getElementById("_2").style.display="inline"},1000);
+  setTimeout(function(){document.getElementById("_3").style.display="inline"},1500);
+  setTimeout(function(){document.getElementById("_4").style.display="inline"},2000);
+  setTimeout(function(){document.getElementById("_5").style.display="inline"},2500);
+  setTimeout(function(){document.getElementById("_6").style.display="inline"},3000);
+  setTimeout(function(){document.getElementById("_7").style.display="inline"},3500);
+  setTimeout(function(){document.getElementById("_8").style.display="inline"},4000);
+  setTimeout(function(){document.getElementById("_9").style.display="inline"},4500);
+  setTimeout(function(){document.getElementById("_10").style.display="inline"},5000);
+
+  document.getElementById("_1").style.display="none";
+  document.getElementById("_2").style.display="none";
+  document.getElementById("_3").style.display="none";
+  document.getElementById("_4").style.display="none";
+  document.getElementById("_5").style.display="none";
+  document.getElementById("_6").style.display="none";
+  document.getElementById("_7").style.display="none";
+  document.getElementById("_8").style.display="none";
+  document.getElementById("_9").style.display="none";
+  document.getElementById("_10").style.display="none";
+  
+}
